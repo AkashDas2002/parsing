1c62d395bb3a3f02070f8946d7806e94046f91fc
Akash Das <adas00@stuy.edu>
2019-11-26 11:33:10 -0500

    add files

---
+++ main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <dirent.h>
+
+
+int main(int argc, char *argv[]){
+ return 0;
+}
